37ad51eb83b5f10e0c0ea7ffd673d7e430d15fc3
Nikky Nalin Parashar <parashar@usf.edu>
2019-02-10 22:09:43 -0500

    Added comments and self-reflection.

--- Assignment2_19/Program.cs
+++ Assignment2_19/Program.cs
@@ -55,7 +55,7 @@ namespace Assignment2_19
             int year = 2017;
             Console.WriteLine(dayOfProgrammer(year));
 
-            Console.WriteLine("Press any key to exit..");
+            Console.WriteLine("\n\nPress any key to exit..");
             Console.ReadKey(true);
 
             /*
@@ -65,26 +65,33 @@ namespace Assignment2_19
              * of data, efficiency becomes extremely importan. Because of that,  we 
              * should keep thinking and try our best to figure out better solutions when
              * solving computational problems.
-             * 
-             * 
             */
-        }
+
+            /*
+             * Self-reflection-Nikky Parashar: It was a good learning, spanning from 
+             * handling array to list and using methods/libraries associated with them.
+             * Eventhough this assigment covers basics of the programming, the wonders 
+             * that can be done in terms of efficiency, logic and modularity is at par.
+             * It would be more fun to deal with different and distinct libraries in 
+             * upcoming assignments.
+            */
+        } //end of Main method
 
         static void displayArray(int[] arr)
         {
-            try
+            try //execute the successfully if the incoming parameter is an array
             {
-                Console.WriteLine();
                 foreach (int n in arr)
                 {
                     Console.Write(n + " ");
                 }
+                Console.WriteLine();
             }
-            catch
+            catch //throw exception if the incoming parameter is not an array
             {
                 Console.WriteLine("Exception occured. Please try again with valid inputs.");
             }
-        }
+        } //end of method: displayArray
 
         // Complete the rotLeft function below.
         static int[] rotLeft(int[] a, int d)
@@ -102,11 +109,11 @@ namespace Assignment2_19
                         if (index >= 0)
                         {
                             b[index] = a[i]; // Replace the index element by the current element.
-                    }
+                        }
                         else
                         {
                             b[place] = a[i]; // Replace the place element by the current element.
-                    }
+                        }
                     }
                     return b; // Return the new array.
                 }
@@ -114,7 +121,7 @@ namespace Assignment2_19
                 {
                    return null;               
                 }                                
-        }
+        } //end of method: rotLeft
 
         // Complete the maximumToys function below.
         static int maximumToys(int[] prices, int k)
@@ -139,7 +146,7 @@ namespace Assignment2_19
                 Console.WriteLine("Exception occured. Please try again with valid inputs.");
                 return 0;
             }
-        }
+        } //end of method: maximumToys
 
         // Complete the balancedSums function below.
         static String balancedSums(List<int> arr)
@@ -162,159 +169,95 @@ namespace Assignment2_19
                 }
                 }            
                 return "NO";  // Else, return NO.
-        }
+        } //end of method: balancedSums
 
         // Complete the missingNumbers function below.
         static int[] missingNumbers(int[] arr, int[] brr)
         {
             {
-                int min = brr[0], max = brr[0], diff = 100;
-                int[] crr = new int[1];
+                int min = brr[0], max = brr[0], diff = 0; //initialize min and max elements of the arrays and difference between them
 
-                var listA = new List<int>(arr);
-                var listB = new List<int>(brr);
-
-                foreach (int i in brr)
+                if (arr.Length > brr.Length) //length of original array must be greater than length of missing array
                 {
-                    if (i > max)
+                    Console.WriteLine("The number of elements in missing array is greater than original array.");
+                    return null; //returns nothing as the condition is not satisfied and throws as exception later
+                }
+
+                var listA = new List<int>(arr); //create list of the elements of missing array
+                var listB = new List<int>(brr); //create list of the elements of original array
+
+                foreach (int i in brr) //loop through each element of original array
+                {
+                    if (i > max) //current element of array must be greater than the current maximum value
                     {
-                        max = i;
+                        max = i; //replace current maximum value with current element of original array
                     }
-                    if (i < min)
+                    if (i < min) //current element of array must be less than the current minimum value
                     {
-                        min = i;
+                        min = i; //replace current minimum value with current element of original array
                     }
                 }
 
-                diff = max - min;
+                diff = max - min; //store the difference between maximum and minimum values of original array
 
-                if (diff <= 100)
+                if (diff <= 100) //difference must be less than or equal to 100
                 {
 
-                    for (int i = 0; i < listA.Count; i++)
+                    for (int i = 0; i < listA.Count; i++) //loop through length of list
                     {
-                        if (listB.Remove(listA[i]))
+                        if (listB.Remove(listA[i])) //if element is found in both lists and is removed from list B (original list)
                         {
-                            listA.RemoveAt(i--);
+                            listA.RemoveAt(i--); //remove the element from list A at the previous position
                         }
                     }
-                    crr = listB.Distinct().ToArray();
-                    crr = SelectionSort(crr);
+                    int[] crr = listB.Distinct().ToArray(); //convert the list into array containing distinct values
+                    crr = SelectionSort(crr); //sort the array in ascending order using Selection Sort method
                     //}
-                    return crr;
+                    return crr; //return sorted array containing missing values
                 }
                 else
                 {
                     Console.WriteLine("The difference between max and min elements of original array is greater than 100");
-                    return null;
+                    return null; //returns nothing as the condition is not satisfied and throws as exception later
                 }
             }
-        }
+        } //end of method: missingNumbers
 
 
         // Complete the gradingStudents function below.
-         static int[] gradingStudents(int[] grades)
+        static int[] gradingStudents(int[] grades)
         {
-            int[] r3 = new int[grades.Length];
-            for (int i = 0; i < grades.Length; i++)
+            int[] r3 = new int[grades.Length]; //creates an array of same length as incoming array
+            for (int i = 0; i < grades.Length; i++) //loop through all the elements of the array
             {
-                if (grades[i] % 5 > 2 && !(grades[i] < 38))
+                if (grades[i] > 100) //cuurent element of the array must be greater than 100
                 {
-                    r3[i] = grades[i] + (5 - grades[i] % 5);
-
+                    Console.WriteLine("Please enter grades between 0 and 100");
+                    return null; //returns nothing as the condition is not satisfied and throws as exception later
+                }
+                else if ((grades[i] % 5 > 2) && !(grades[i] < 38)) //current element must have remainder greater than 2 on dividing by 5 and is not less than 38
+                {
+                    r3[i] = grades[i] + (5 - grades[i] % 5); //insert value in the resultant array after rounding it to next multiple of 5
                 }
                 else
                 {
-                    r3[i] = grades[i];
-                    
+                    r3[i] = grades[i]; //insert value in the resultant array after rounding it to next multiple of 5
                 }
-                
             }
-            
-            return r3;
-            
-        }
+            return r3; //return resultant array with rounded values, wherever needed
+        } //end of method: gradingStudents
 
         // Complete the findMedian function below.
-        static int partition(int[] arr, int low, int high)
+        static int findMedian(int[] arr) 
         {
-            int pivot = arr[low];
-            int i = low, pivotIndex = low, j = high, ctr = 0, temp=0;
+            int pos_med = 0; //intialize index of median in array 
+            arr = SelectionSort(arr); //sort the array in ascending order using Selection Sort method
 
-            while (i <= j)
-            {
-                if (arr[i] < pivot)
-                {
-                    temp = arr[i];
-                    arr[i] = arr[pivotIndex];
-                    arr[pivotIndex] = temp;
-                    pivotIndex++; i++;
-                }
-                else if (arr[i] > pivot)
-                {
-                    temp = arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-                    j--;
+            pos_med = (arr.Length - 1) / 2; //store the index of median based on the middle value of the array
 
-                }
-                if (arr[i] == pivot)
-                {
-                    i++; ctr++;
-                }
-            }
+            return arr[pos_med]; //retunr median of the array
 
-            return pivotIndex;
-        }
-        /*static int selection_algorithm(int[] arr, int left, int right, int kth)
-        {
-            for (; ; )
-            {
-                int pivotIndex = partition(arr, left, right);
-                int len = pivotIndex - left + 1;
-
-                if (kth == len)
-                    return arr[pivotIndex];
-
-                else if (kth < len)
-                    right = pivotIndex - 1;
-
-                else
-                {
-                    kth = kth - len;
-                    left = pivotIndex + 1;
-                }
-                
-            }
-
-        }*/
-        
-        static int findMedian(int[] arr)
-        {
-            int n = arr.Length, left = 0, right = n-1;
-            int kth = n/2 + 1;
-
-            for (; ; )
-            {
-                int pivotIndex = partition(arr, left, right);
-                int len = pivotIndex - left + 1;
-
-                if (kth == len)
-                    return arr[pivotIndex];
-
-                else if (kth < len)
-                    right = pivotIndex - 1;
-
-                else
-                {
-                    kth = kth - len;
-                    left = pivotIndex + 1;
-                }
-                
-            }
-
-            //return selection_algorithm(arr, 0, n - 1, n / 2 + 1);
-        }
+        } //end of method: findMedian
 
         // Complete the closestNumbers function below.
         static int[] closestNumbers(int[] arr)
@@ -335,94 +278,98 @@ namespace Assignment2_19
                 }
             }
             return b.ToArray(); // Return the new array.
-        }
+        } //end of method: closestNumbers
 
         // Complete the dayOfProgrammer function below.
         static string dayOfProgrammer(int year)
         {
-            bool isLeap = false;
+            bool isLeap = false; //initialize value of year is leap or not to false
             string date;
             double month = 0;
-            int day = 0, days = 0;
-            int[] monthsLeap = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-            int[] months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int day = 0, days = 0; //day id "dd" in "dd/mm/yyyy"; days is sum of days for dynamic number of months
+            int[] monthsLeap = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //array containing month's days in leap year
+            int[] months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; //array containing month'sdays in non-leap year
 
-            if (year == 1918)
+            if (year == 1918) //year must be 1918
             {
-                month = Math.Ceiling((double)(256) / (double)(30));
+                month = Math.Ceiling((double)(256) / (double)(30)); //stores month
 
-                for (int i = 0; i < month - 1; i++)
+                for (int i = 0; i < month - 1; i++) //loops through January to previous month of stored month
                 {
-                    days += months[i];
+                    days += months[i]; //add number of days for each month
                 }
 
-                day = 256 - days + 13;
+                day = 256 - days + 13; //store day left after subtracting sum of days and 13 days (since 14th Feb was 32nd day of the year) from 256
             }
             else
             {
-                if ((year > 1699) && (year < 1918))
+                if ((year > 1699) && (year < 1918)) //year must be between 1700 and 1918
                 {
-                    if ((year % 4) == 0)
+                    if ((year % 4) == 0) //check if entered year is a leap year as per julian calendar
                     {
-                        isLeap = true;
+                        isLeap = true; //set value of boolean isLeap to true
                     }
                 }
-                else if ((year > 1918) && (year < 2701))
+                else if ((year > 1918) && (year < 2701)) //year must be between 1918 and 2700
                 {
-                    if (((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0)))
+                    if (((year % 400) == 0) || (((year % 4) == 0) && ((year % 100) != 0))) //check if entered year is a leap year as per gregorian calendar
                     {
-                        isLeap = true;
+                        isLeap = true; //set value of boolean isLeap to true
                     }
                 }
                 else
                 {
-                    return "Invalid year entered. Please enter year between 1700 and 2700.";
+                    return "Invalid year entered. Please enter year between 1700 and 2700."; //year is outside range of 1700 and 2700
                 }
-                if (isLeap)
+                if (isLeap) //year is a leap year
                 {
-                    month = Math.Ceiling((double)(256) / (double)(30));
-                    for (int i = 0; i < month - 1; i++)
+                    month = Math.Ceiling((double)(256) / (double)(30)); //stores month
+
+                    for (int i = 0; i < month - 1; i++) //loops through January to previous month of stored month
                     {
-                        days += monthsLeap[i];
+                        days += monthsLeap[i]; //add number of days for each month of leap year array
                     }
-                    day = 256 - days;
+                    day = 256 - days; //store day left after subtracting sum of days from 256
                 }
-                else
+                else //year is not a leap year
                 {
-                    month = Math.Ceiling((double)(256) / (double)(30));
-                    for (int i = 0; i < month - 1; i++)
+                    month = Math.Ceiling((double)(256) / (double)(30)); //stores month
+
+                    for (int i = 0; i < month - 1; i++) //loops through January to previous month of stored month
                     {
-                        days += months[i];
+                        days += months[i]; //add number of days for each month of leap year array
                     }
-                    day = 256 - days;
+                    day = 256 - days; //store day left after subtracting sum of days from 256
                 }
             }
 
-            date = Convert.ToString(day) + ".0" + Convert.ToString(month) + "." + Convert.ToString(year);
-            return date;
-        }
-        static int[] SelectionSort(int[] arr2)
+            date = Convert.ToString(day) + ".0" + Convert.ToString(month) + "." + Convert.ToString(year); // store date in format of dd.mm.yyyy
+            return date; //returns date for 256th day of the year
+        } //end of method: dayOfProgrammer
+
+        //sorts array in ascending order using Selection Sort method
+        static int[] SelectionSort(int[] arr2) 
         {
             int min, temp;
 
-            for (int i = 0; i < arr2.Length - 1; i++)
+            for (int i = 0; i < arr2.Length - 1; i++) //loop through length of array
             {
-                min = i;
-                for (int j = i + 1; j < arr2.Length; j++)
+                min = i; //initialize minimum value position to current index of array
+                for (int j = i + 1; j < arr2.Length; j++) //loop through length of array starting from 2nd position
                 {
-                    if (arr2[j] < arr2[min])
+                    if (arr2[j] < arr2[min]) //cuurent element of inner loop must be less than element at minimum element's position
                     {
-                        min = j;
+                        min = j; //replace minimum value position to current index of array of inner loop
                     }
                 }
-                if (min != i)
+                if (min != i) //minimum value position must not be equal to current index of array of outer loop
                 {
-                    temp = arr2[i];
-                    arr2[i] = arr2[min];
-                    arr2[min] = temp;
+                    temp = arr2[i]; //store element at current index of array of outer loop in temporary variable
+                    arr2[i] = arr2[min]; //replace element at current index of array of outer loop with element at minimum element's position
+                    arr2[min] = temp; //replace element at minimum element's position with temporary variable
                 }
             }
-            return arr2;
-        }
-    }
-}
+            return arr2; //return sorted array in ascending order
+        } //end of method: SelectionSort
+    } //end of class: Program
+} //end of namespace: Assignment2_19
